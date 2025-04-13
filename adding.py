2f98eedc4eaad620e6333ed18b98e4e89a6c4363
Affan Fareed <1912561-AffanFareed1@users.noreply.replit.com>
2025-04-13 04:21:26 +0000

    Initial commit

---
+++ adding.py
@@ -0,0 +1,8 @@
+def add(a, b):
+  result = a + b
+  return result
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("The sum is:", add(num1, num2))
---
+++ countdown.py
@@ -0,0 +1,8 @@
+import time
+
+# Countdown from 10
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(0.5)
+
+print("Happy New Year!")
